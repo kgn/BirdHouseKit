81515051bd8de80e9571eb1394c5909aa98c7f6d
David Keegan <git@davidkeegan.com>
2011-12-28 12:10:56 -0800

    initial commit

---
+++ BirdHouseKit/BirdHouseKit.h
@@ -0,0 +1,13 @@
+//
+//  BirdHouseKit.h
+//  BirdHouseKit
+//
+//  Created by David Keegan on 12/28/11.
+//  Copyright (c) 2011 David Keegan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BirdHouseKit : NSObject
+
+@end
---
+++ BirdHouseKit/BirdHouseKit.m
@@ -0,0 +1,13 @@
+//
+//  BirdHouseKit.m
+//  BirdHouseKit
+//
+//  Created by David Keegan on 12/28/11.
+//  Copyright (c) 2011 David Keegan. All rights reserved.
+//
+
+#import "BirdHouseKit.h"
+
+@implementation BirdHouseKit
+
+@end
